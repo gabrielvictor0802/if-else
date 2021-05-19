9335b4b29ce3652cc16065d2d9ca6900eec4b450
gabrielvicto0802 <gabrirlvictor0802@gmail.com>
2021-05-19 18:29:17 -0300

    suindo if/esle

---
+++ index.js
@@ -0,0 +1,21 @@
+let idade= 19
+
+if (idade > 18) {
+    console.log(`Parabens, agora ja pode ser preso kkk `)
+
+}
+
+let gabriel = 19
+
+if (gabriel >= 18) {
+    console.log (`Como sou de maior deu true`)
+}
+
+
+let mes1 = "janeiro"
+let mes2 = "Dezembro"
+let mes3 = "Fevereiro"
+
+if (mes1 !== mes3 && mes2 !== mes3) {
+    console.log( "infelizmente ainda não estamos no mes do seu anivesario")
+}
